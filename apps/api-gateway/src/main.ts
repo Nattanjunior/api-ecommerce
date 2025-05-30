9fe9f2d9187cfa4d6505b48f5a8804d3f8aa76e4
nattanjunior <fnatanieljunior@gmail.com>
2025-05-30 02:44:40 -0300

    feat: configura API Gateway para usar RabbitMQ como sistema de mensageria

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
-import { ApiGatewayModule } from './api-gateway.module';
+import { AppModule } from '../../app.module';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(ApiGatewayModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://localhost:5672'],
+      queue: 'api-gateway',
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(3000);
+  console.log('API Gateway is running on port 3000');
 }
 bootstrap();
