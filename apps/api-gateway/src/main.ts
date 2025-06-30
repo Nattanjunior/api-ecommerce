95fc421510086bb7e94cc19bfb3499bfca9f38b4
nattanjunior <fnatanieljunior@gmail.com>
2025-06-30 15:54:11 -0300

    chore(api-gateway): adiciona configuração do Swagger para documentação da API

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,20 +1,30 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from '../../app.module';
+import { ApiGatewayModule } from './api-gateway.module';
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(ApiGatewayModule);
 
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.RMQ,
     options: {
-      urls: ['amqp://localhost:5672'],
+      urls: ['amqp://rabbitmq:5672'],
       queue: 'api-gateway',
       queueOptions: {
         durable: false,
       },
     },
   });
+
+  // Configuração do Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API E-commerce')
+    .setDescription('Documentação da API do e-commerce')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
 
   await app.startAllMicroservices();
   await app.listen(3000);
