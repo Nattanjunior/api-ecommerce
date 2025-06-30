93ea2d25d69eeb72c6487e46a7f152c8816e8a58
nattanjunior <fnatanieljunior@gmail.com>
2025-06-30 18:25:23 -0300

    feat(payment-service): expõe endpoints de pagamento (pix, boleto, cartão) via microserviço

--- apps/payment-service/src/payment-service.controller.ts
+++ apps/payment-service/src/payment-service.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { PaymentServiceService } from './payment-service.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class PaymentServiceController {
   constructor(private readonly paymentServiceService: PaymentServiceService) {}
 
-  @Get()
-  getHello(): string {
-    return this.paymentServiceService.getHello();
+  @MessagePattern({ cmd: 'pay-with-card' })
+  payWithCard(@Payload() data: { amount: number; currency: string; cardDetails: any }) {
+    return this.paymentServiceService.payWithCard(data.amount, data.currency, data.cardDetails);
+  }
+
+  @MessagePattern({ cmd: 'pay-with-boleto' })
+  payWithBoleto(@Payload() data: { amount: number; currency: string; customer: any }) {
+    return this.paymentServiceService.payWithBoleto(data.amount, data.currency, data.customer);
+  }
+
+  @MessagePattern({ cmd: 'pay-with-pix' })
+  payWithPix(@Payload() data: { amount: number; currency: string; customer: any }) {
+    return this.paymentServiceService.payWithPix(data.amount, data.currency, data.customer);
   }
 }
